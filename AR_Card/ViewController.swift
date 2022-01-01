99b9fb8ed062c04e2dd49086b28e0d93e9220e23
godo <godo129@naver.com>
2022-01-01 21:44:10 +0900

    Initial Commit

---
+++ AR_Card/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  AR_Card
+//
+//  Created by hong on 2022/01/01.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+     
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+}
+

6e4e29d91368600d01a83b41ed62e91e3f298cc3
godo <godo129@naver.com>
2022-01-03 23:13:28 +0900

    UITextField 한계 설정

--- AR_Card/Controller/CardAddViewController.swift
+++ AR_Card/Controller/CardAddViewController.swift
@@ -19,13 +19,16 @@ class CardAddViewController: UIViewController {
     
     private let firstNumTextField: UITextField = {
         let firstNumTextField = UITextField()
-        firstNumTextField.keyboardType = .numberPad
+        firstNumTextField.layer.borderColor = UIColor.black.cgColor
+        firstNumTextField.layer.borderWidth = 2
         return firstNumTextField
     }()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        firstNumTextField.delegate = self
         
         view.addSubview(Title)
         view.addSubview(firstNumTextField)
@@ -36,7 +39,31 @@ class CardAddViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
         Title.frame = CGRect(x: 20, y: 20, width: 200, height: 40)
-        firstNumTextField.frame = CGRect(x: 20, y: 100, width: 50, height: 50)
+        firstNumTextField.frame = CGRect(x: 20, y: 100, width: 200, height: 50)
     }
     
+}
+
+//MARK: - UITextField Delegate
+// UITextField 한계 설정 
+extension CardAddViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let cs = NSCharacterSet(charactersIn: "0123456789").inverted
+        let filtered = string.components(separatedBy: cs).joined(separator: "")
+        
+        // 백스페이스
+        if string.isEmpty {
+            return true
+        }
+        
+        // 텍스트 길이 범주
+        if textField.text!.count > 3 {
+            return false
+        }
+        
+        return (string == filtered)
+        
+    }
+
 }

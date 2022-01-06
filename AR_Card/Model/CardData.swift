1abff6cc202c16cb435f086d91a4aa152373f908
godo <godo129@naver.com>
2022-01-06 10:33:19 +0900

    데이터 정리

--- AR_Card/Model/CardData.swift
+++ AR_Card/Model/CardData.swift
@@ -6,19 +6,6 @@
 //
 
 import Foundation
-import UIKit
-
-
-var myCard = cardManager(cardFirstNumber: "0000",
-                         cardSecondNumber: "0000",
-                         cardThirdNumber: "0000",
-                         cardFourthNumber: "0000",
-                         monthOfLimit: "01",
-                         yearOfLimit: "01",
-                         cardName: "내카드1",
-                         CVCNumber: "789",
-                         bankName: "국민",
-                         cardImage: UIImage(named: "image1")!)
 
 var myCardList = [cardManager]()
 

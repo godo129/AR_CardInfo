84ccc2f6b4e45cd82b60f11d8975887540d2c67c
godo <godo129@naver.com>
2022-01-06 10:35:04 +0900

    테이블 뷰 데이터 뿌려주고 삭제, 수정 기능 추가

--- AR_Card/Controller/CardInfoViewController.swift
+++ AR_Card/Controller/CardInfoViewController.swift
@@ -11,41 +11,92 @@ class CardInfoViewController: UIViewController {
     
     private let CardInfoTable = UITableView()
     
-    private let Title: UILabel = {
-        let title = UILabel()
-        title.text = "카드 정보"
-        title.textColor = .black
-        title.font = .systemFont(ofSize: 30)
-        return title
+    private let header: UIView = {
+        let header = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+        
+        let headerLabel = UILabel(frame: header.bounds)
+        headerLabel.text = "카드 관리"
+        headerLabel.font = .systemFont(ofSize: 30, weight: .bold)
+        header.addSubview(headerLabel)
+        return header
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        CardInfoTable.allowsSelection = false
+        CardInfoTable.tableHeaderView = header
         
         CardInfoTable.register(UITableViewCell.self, forCellReuseIdentifier: "cardCell")
         
         CardInfoTable.delegate = self
         CardInfoTable.dataSource = self
 
-        view.addSubview(Title)
         view.addSubview(CardInfoTable)
+        
+        let noti = NotificationCenter.default
+        noti.addObserver(self, selector: #selector(myCardInfoUpdate), name: myCardInfoUpdateNoti, object: nil)
+    }
+    
+    @objc private func myCardInfoUpdate() {
+        CardInfoTable.reloadData()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.post(name: myCardInfoUpdateNoti, object: nil)
     }
  
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        Title.frame = CGRect(x: 20, y: 20, width: 200, height: 40)
         CardInfoTable.frame = view.bounds
     }
 
 }
 extension CardInfoViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = CardInfoTable.dequeueReusableCell(withIdentifier: "cardCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cardCell", for: indexPath)
+        cell.imageView?.image = myCardList[indexPath.row].cardImage
+        cell.textLabel?.text = myCardList[indexPath.row].cardName
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return myCardList.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return view.frame.height/5
+    }
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let removeButton = UITableViewRowAction(style: .normal, title: "삭제") { (rowAction, indexpath) in
+            myCardList.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+        removeButton.backgroundColor = .red
+        
+        let editButton = UITableViewRowAction(style: .normal, title: "수정") { (rowAction, indexpath) in
+            myCardListIdx = indexpath.row
+            
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "AddCardView")
+            vc?.modalTransitionStyle = .flipHorizontal
+            self.present(vc!, animated: false, completion: nil)
+            
+        }
+        editButton.backgroundColor = .orange
+        
+        let cancelButton = UITableViewRowAction(style: .normal, title: "취소") { (rowAction, indexpath) in
+            print("cancel button tapped")
+        }
+        cancelButton.backgroundColor = .green
+        
+        return [cancelButton,editButton,removeButton]
     }
 }

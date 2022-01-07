ed6e6bef36525bb610c200d24415d436677dbd2d
godo <godo129@naver.com>
2022-01-07 11:59:05 +0900

    디자인 및 텍스트 필드 제한 문자 설정

--- AR_Card/Controller/CardAddViewController.swift
+++ AR_Card/Controller/CardAddViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import TextFieldEffects
+import SnapKit
 
 class CardAddViewController: UIViewController {
     
@@ -14,20 +16,11 @@ class CardAddViewController: UIViewController {
                              cardThirdNumber: "0000",
                              cardFourthNumber: "0000",
                              monthOfLimit: "01",
-                             yearOfLimit: "01",
+                             yearOfLimit: "22",
                              cardName: "내카드",
                              CVCNumber: "000",
                              bankName: "00은행",
                              cardImage: UIImage(named: "image1")!)
-    
-
-    private let imageChangeButton: UIButton = {
-        let imageChangeButton = UIButton()
-        imageChangeButton.setTitle("사진 선택", for: .normal)
-        imageChangeButton.setTitleColor(.black, for: .normal)
-        imageChangeButton.backgroundColor = .orange
-        return imageChangeButton
-    }()
     
     private let cardImageView: UIImageView = {
         let cardImageView = UIImageView()
@@ -43,76 +36,81 @@ class CardAddViewController: UIViewController {
         return title
     }()
     
-    private let firstNumTextField: UITextField = {
-        let firstNumTextField = UITextField()
-        firstNumTextField.layer.borderColor = UIColor.black.cgColor
-        firstNumTextField.layer.borderWidth = 2
+    private let firstNumTextField: HoshiTextField = {
+        let firstNumTextField = HoshiTextField()
+        firstNumTextField.borderActiveColor = .gray
+        firstNumTextField.textAlignment = .center
         firstNumTextField.keyboardType = .numberPad
         return firstNumTextField
     }()
     
-    private let secondNumTextField: UITextField = {
-        let secondNumTextField = UITextField()
-        secondNumTextField.layer.borderColor = UIColor.black.cgColor
-        secondNumTextField.layer.borderWidth = 2
+    private let secondNumTextField: HoshiTextField = {
+        let secondNumTextField = HoshiTextField()
+        secondNumTextField.borderActiveColor = .gray
+        secondNumTextField.textAlignment = .center
         secondNumTextField.keyboardType = .numberPad
         return secondNumTextField
     }()
     
-    private let thirdNumTextField: UITextField = {
-        let thirdNumTextField = UITextField()
-        thirdNumTextField.layer.borderColor = UIColor.black.cgColor
-        thirdNumTextField.layer.borderWidth = 2
+    private let thirdNumTextField: HoshiTextField = {
+        let thirdNumTextField = HoshiTextField()
+        thirdNumTextField.borderActiveColor = .gray
+        thirdNumTextField.textAlignment = .center
         thirdNumTextField.keyboardType = .numberPad
         return thirdNumTextField
     }()
     
-    private let fourthNumTextField: UITextField = {
-        let fourthNumTextField = UITextField()
-        fourthNumTextField.layer.borderColor = UIColor.black.cgColor
-        fourthNumTextField.layer.borderWidth = 2
+    private let fourthNumTextField: HoshiTextField = {
+        let fourthNumTextField = HoshiTextField()
+        fourthNumTextField.borderActiveColor = .gray
+        fourthNumTextField.textAlignment = .center
         fourthNumTextField.keyboardType = .numberPad
         return fourthNumTextField
     }()
     
-    private let monthOfLimitTextField: UITextField = {
-        let monthOfLimitTextField = UITextField()
-        monthOfLimitTextField.layer.borderColor = UIColor.black.cgColor
-        monthOfLimitTextField.layer.borderWidth = 2
+    private let monthOfLimitTextField: HoshiTextField = {
+        let monthOfLimitTextField = HoshiTextField()
+        monthOfLimitTextField.placeholder = "MONTH"
+        monthOfLimitTextField.placeholderColor = .lightGray
+        monthOfLimitTextField.borderActiveColor = .gray
         monthOfLimitTextField.keyboardType = .numberPad
         monthOfLimitTextField.tag = 1
         return monthOfLimitTextField
     }()
     
-    private let yearOfLimitTextField: UITextField = {
-        let yearOfLimitTextField = UITextField()
-        yearOfLimitTextField.layer.borderColor = UIColor.black.cgColor
-        yearOfLimitTextField.layer.borderWidth = 2
+    private let yearOfLimitTextField: HoshiTextField = {
+        let yearOfLimitTextField = HoshiTextField()
+        yearOfLimitTextField.placeholder = "YEAR"
+        yearOfLimitTextField.placeholderColor = .lightGray
+        yearOfLimitTextField.borderActiveColor = .gray
         yearOfLimitTextField.keyboardType = .numberPad
         yearOfLimitTextField.tag = 2
         return yearOfLimitTextField
     }()
     
-    private let cvcNumTextField: UITextField = {
-        let cvcNumTextField = UITextField()
-        cvcNumTextField.layer.borderColor = UIColor.black.cgColor
-        cvcNumTextField.layer.borderWidth = 2
+    private let cvcNumTextField: HoshiTextField = {
+        let cvcNumTextField = HoshiTextField()
+        cvcNumTextField.placeholder = "CVC"
+        cvcNumTextField.placeholderColor = .lightGray
+        cvcNumTextField.borderActiveColor = .gray
         cvcNumTextField.keyboardType = .numberPad
         cvcNumTextField.tag = 3
         return cvcNumTextField
     }()
     
-    private let bankNameTextField: UITextField = {
-        let bankNameTextField = UITextField()
-        bankNameTextField.layer.borderColor = UIColor.black.cgColor
-        bankNameTextField.layer.borderWidth = 2
+    private let bankNameTextField: HoshiTextField = {
+        let bankNameTextField = HoshiTextField()
+        bankNameTextField.placeholder = "은행"
+        bankNameTextField.placeholderColor = .lightGray
+        bankNameTextField.borderActiveColor = .gray
         return bankNameTextField
     }()
     
-    private let cardNameTextField: UITextField = {
-        let cardNameTextField = UITextField()
-        cardNameTextField.layer.borderColor = UIColor.black.cgColor
-        cardNameTextField.layer.borderWidth = 2
+    private let cardNameTextField: HoshiTextField = {
+        let cardNameTextField = HoshiTextField()
+        cardNameTextField.placeholder = "카드 별명"
+        cardNameTextField.placeholderColor = .lightGray
+        cardNameTextField.borderActiveColor = .gray
         return cardNameTextField
     }()
     
@@ -120,8 +118,21 @@ class CardAddViewController: UIViewController {
         let recordButton = UIButton()
         recordButton.setTitle("저장", for: .normal)
         recordButton.setTitleColor(.black, for: .normal)
-        recordButton.backgroundColor = .orange
+        recordButton.layer.borderWidth = 1.5
+        recordButton.layer.cornerRadius = 20
+        recordButton.layer.borderColor = UIColor.systemBlue.cgColor
         return recordButton
+    }()
+    
+    
+    private let imageChangeButton: UIButton = {
+        let imageChangeButton = UIButton()
+        imageChangeButton.setTitle("사진 선택", for: .normal)
+        imageChangeButton.setTitleColor(.black, for: .normal)
+        imageChangeButton.layer.borderWidth = 1.5
+        imageChangeButton.layer.borderColor = UIColor.systemTeal.cgColor
+        imageChangeButton.layer.cornerRadius = 15
+        return imageChangeButton
     }()
 
     override func viewDidLoad() {
@@ -249,21 +260,103 @@ class CardAddViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        Title.frame = CGRect(x: 20, y: 20, width: 200, height: 40)
-        firstNumTextField.frame = CGRect(x: 20, y: 100, width: 70, height: 50)
-        secondNumTextField.frame = CGRect(x: 120, y: 100, width: 70, height: 50)
-        thirdNumTextField.frame = CGRect(x: 220, y: 100, width: 70, height: 50)
-        fourthNumTextField.frame = CGRect(x: 320, y: 100, width: 70, height: 50)
-        monthOfLimitTextField.frame = CGRect(x: 200, y: 200, width: 50, height: 50)
-        yearOfLimitTextField.frame = CGRect(x: 250, y: 200, width: 50, height: 50)
+//        Title.frame = CGRect(x: 20, y: 20, width: 200, height: 40)
+        Title.snp.makeConstraints { make in
+            make.left.equalTo(self.view.safeAreaLayoutGuide).offset(10)
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(20)
+            make.size.equalTo(CGSize(width: 200, height: 40))
+        }
         
-        cvcNumTextField.frame = CGRect(x: 20, y: 200, width: 100, height: 50)
-        bankNameTextField.frame = CGRect(x: 20, y: 250, width: 100, height: 50)
-        cardNameTextField.frame = CGRect(x: 20, y: 300, width: 100, height: 50)
-        recordButton.frame = CGRect(x: 100, y: 250, width: 100, height: 50)
+//        recordButton.frame = CGRect(x: 260, y: 20, width: 100, height: 40)
+        recordButton.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(20)
+            make.right.equalTo(self.view.safeAreaLayoutGuide).offset(-15)
+            make.size.equalTo(CGSize(width: 100, height: 40))
+        }
         
-        imageChangeButton.frame = CGRect(x: 200, y: 250, width: 100, height: 50)
-        cardImageView.frame = CGRect(x: 300, y: 300, width: 200, height: 200)
+        
+//        firstNumTextField.frame = CGRect(x: 20, y: 100, width: 70, height: 50)
+//        secondNumTextField.frame = CGRect(x: 120, y: 100, width: 70, height: 50)
+//        thirdNumTextField.frame = CGRect(x: 220, y: 100, width: 70, height: 50)
+//        fourthNumTextField.frame = CGRect(x: 320, y: 100, width: 70, height: 50)
+        
+        firstNumTextField.snp.makeConstraints { make in
+            make.top.equalTo(Title).offset(60)
+            make.left.equalTo(self.view.safeAreaLayoutGuide).offset(20)
+            make.size.equalTo(CGSize(width: 70, height: 50))
+        }
+        
+        secondNumTextField.snp.makeConstraints { make in
+            make.top.equalTo(Title).offset(60)
+            make.left.equalTo(firstNumTextField).offset(90)
+            make.size.equalTo(CGSize(width: 70, height: 50))
+        }
+        
+        thirdNumTextField.snp.makeConstraints { make in
+            make.top.equalTo(Title).offset(60)
+            make.left.equalTo(secondNumTextField).offset(90)
+            make.size.equalTo(CGSize(width: 70, height: 50))
+        }
+        
+        fourthNumTextField.snp.makeConstraints { make in
+            make.top.equalTo(Title).offset(60)
+            make.left.equalTo(thirdNumTextField).offset(90)
+            make.size.equalTo(CGSize(width: 70, height: 50))
+        }
+//
+//        monthOfLimitTextField.frame = CGRect(x: 20, y: 150, width: 50, height: 50)
+//        yearOfLimitTextField.frame = CGRect(x: 70, y: 150, width: 50, height: 50)
+        
+        monthOfLimitTextField.snp.makeConstraints { make in
+            make.top.equalTo(firstNumTextField).offset(60)
+            make.left.equalTo(firstNumTextField)
+            make.size.equalTo(CGSize(width: 50, height: 50))
+        }
+        
+        yearOfLimitTextField.snp.makeConstraints { make in
+            make.top.equalTo(monthOfLimitTextField)
+            make.left.equalTo(monthOfLimitTextField).offset(50)
+            make.size.equalTo(monthOfLimitTextField)
+        }
+//
+//        cvcNumTextField.frame = CGRect(x: 20, y: 200, width: 100, height: 50)
+//        bankNameTextField.frame = CGRect(x: 20, y: 250, width: 100, height: 50)
+//        cardNameTextField.frame = CGRect(x: 20, y: 300, width: 100, height: 50)
+        
+        cvcNumTextField.snp.makeConstraints { make in
+            make.top.equalTo(monthOfLimitTextField).offset(55)
+            make.left.equalTo(monthOfLimitTextField)
+            make.size.equalTo(CGSize(width: 100, height: 50))
+        }
+        bankNameTextField.snp.makeConstraints { make in
+            make.top.equalTo(cvcNumTextField).offset(55)
+            make.left.equalTo(monthOfLimitTextField)
+            make.size.equalTo(CGSize(width: 100, height: 50))
+        }
+        cardNameTextField.snp.makeConstraints { make in
+            make.top.equalTo(bankNameTextField).offset(55)
+            make.left.equalTo(monthOfLimitTextField)
+            make.size.equalTo(CGSize(width: 100, height: 50))
+        }
+        
+//        cardImageView.frame = CGRect(x: 140, y: 170, width: 200, height: 150)
+        
+        cardImageView.snp.makeConstraints { make in
+            make.top.equalTo(monthOfLimitTextField)
+            make.left.equalTo(yearOfLimitTextField).offset(55)
+            make.right.equalTo(fourthNumTextField)
+            make.bottom.equalTo(cardNameTextField).offset(-50)
+        }
+
+//        imageChangeButton.frame = CGRect(x: 200, y: 340, width: 100, height: 30)
+        
+        imageChangeButton.snp.makeConstraints { make in
+            make.top.equalTo(bankNameTextField).offset(70)
+            make.left.equalTo(secondNumTextField).offset(90)
+            make.right.equalTo(fourthNumTextField).offset(-50)
+            make.bottom.equalTo(cardNameTextField)
+        }
+        
     }
 
     
@@ -275,9 +368,27 @@ extension CardAddViewController: UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
-        func textFieldOption(length: Int) -> Bool {
-            let cs = NSCharacterSet(charactersIn: "0123456789").inverted
+        func limitCharacters(characters: String) -> Bool {
+            let cs = NSCharacterSet(charactersIn: characters).inverted
             let filtered = string.components(separatedBy: cs).joined(separator: "")
+            return (string == filtered)
+        }
+        
+        func textFieldOption(length: Int) -> Bool {
+            if textField.tag == 1 {
+                if textField.text!.count == 0{
+                    
+                    return limitCharacters(characters: "01")
+                } else {
+                    if textField.text == "1" {
+                        
+                        return limitCharacters(characters: "012")
+                    } else if textField.text == "0" {
+                        
+                        return limitCharacters(characters: "123456789")
+                    }
+                }
+            }
             
             // 백스페이스
             if string.isEmpty {
@@ -287,7 +398,7 @@ extension CardAddViewController: UITextFieldDelegate {
             if textField.text!.count > length {
                 return false
             }
-            return (string == filtered)
+            return limitCharacters(characters: "0123456789")
         }
         
         if textField.tag == 1 || textField.tag == 2 {
